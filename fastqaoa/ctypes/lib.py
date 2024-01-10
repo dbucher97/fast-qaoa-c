c0e467b0582e9fe631a05cdc85b82bc39ef8b29d
David Bucher <d.bucher97@gmail.com>
2024-01-10 19:58:59 +0100

    dynamic path

--- fastqaoa/ctypes/lib.py
+++ fastqaoa/ctypes/lib.py
@@ -2,6 +2,7 @@ import os
 import ctypes as C
 import numpy as np
 from sys import platform
+from pathlib import Path
 
 __libname = "libqaoa"
 
@@ -22,6 +23,8 @@ elif platform == "darwin":
 else:
     raise RuntimeError(f"Platform {platform} not supported.")
 
-_lib = C.CDLL(f"build/{__libname}.{__ext}")
+path = Path(__file__).absolute().parent.parent.parent
+_lib = C.CDLL(path / "build" / f"{__libname}.{__ext}")
+del path
 
 __all__ = ["_lib", "C", "NP_REAL", "NP_COMPLEX"]
