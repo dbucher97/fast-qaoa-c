600250e40547d65d1f2af324162aa24eba7a17bd
David Bucher <d.bucher97@gmail.com>
2025-03-01 11:48:44 +0100

    fine tune until 20

--- problems/fine_tune.py
+++ problems/fine_tune.py
@@ -6,11 +6,13 @@ from fastqaoa.ctypes.optimize import optimize_qaoa_lbfgs
 from fastqaoa.ctypes.qpe_qaoa import qpe_qaoa_norm
 from fastqaoa.indicator import interpolate_diagonals, get_indicator_interpolator
 from problems.knapsack import Knapsack
+import datetime
+import time
 
 
 SHIFT = 0.4
 INTERPOLATORS = {
-    M: get_indicator_interpolator(M, 4, SHIFT) for M in [2, 4, 6, 8, 12, 16]
+    M: get_indicator_interpolator(M, 4, SHIFT) for M in [4, 6, 8, 12, 16]
 }
 
 INSTANCES = Knapsack.get_instances()
@@ -19,14 +21,13 @@ INSTANCES = Knapsack.get_instances()
 df = pd.read_feather("results/qpe_main2.feather").query("qaoa == 'masked_cost'")
 
 
-exec = ThreadPoolExecutor(max_workers=8)
+exec = ThreadPoolExecutor(max_workers=24)
 
 
 def finetune_instance(x):
     (size, idx), data = x
-    if size > 10:
-        return []
-    print(size, idx)
+    print("started", size, idx, datetime.datetime.now())
+    start = time.time()
     instance: Knapsack = INSTANCES[size][idx]
     f, g = instance.diagonalized()
     feas = g >= 0
@@ -42,14 +43,14 @@ def finetune_instance(x):
         for _, row in data.sort_values(by="depth").iterrows():
             a = perf_counter()
             res = optimize_qaoa_lbfgs(
-                f, cost, row.betas, row.gammas, constr=sgn, maxiter=10
+                f, cost, row.betas, row.gammas, constr=sgn, maxiter=5
             )
             delta = perf_counter() - a
             sv, p, q = qpe_qaoa_norm(f, sgn, res.betas, res.gammas)
             new_data = dict(row)
 
             new_data.update(Metrics.compute(sv, cost, feas).dump())
-            new_data["status"] = res.status
+            new_data["status"] = res.status.name
             new_data["iterations"] = res.it
             new_data["runtime"] = delta
             new_data["betas"] = res.betas
@@ -62,6 +63,8 @@ def finetune_instance(x):
             ret.append(new_data)
 
     instance.decache()
+    elapsed = datetime.timedelta(seconds=time.time() - start)
+    print("finished", size, idx, elapsed)
 
     return ret
 
@@ -69,5 +72,11 @@ collected = []
 for result in exec.map(finetune_instance, df.groupby(["n_qubits", "problem_id"])):
     collected += result
 
-df = pd.concat([df, pd.DataFrame(collected)]).reset_index()
-df.to_feather("results/qpe_main2_ft.feather")
+    if len(collected) >= 12 * 24:
+        df = pd.concat([df, pd.DataFrame(collected)], ignore_index=True)
+        collected = []
+        df.to_feather("results/qpe_main2_ft.feather")
+
+if len(collected) > 0:
+    df = pd.concat([df, pd.DataFrame(collected)], ignore_index=True)
+    df.to_feather("results/qpe_main2_ft.feather")
